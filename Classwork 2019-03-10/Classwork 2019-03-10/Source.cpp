33add6b437b07c6ed3fbeaa830866f33859bdb92
Jarrenstyle <55443245+Jarrenstyle@users.noreply.github.com>
2019-10-03 08:39:26 -0400

    Add files via upload

---
+++ Classwork 2019-03-10/Classwork 2019-03-10/Source.cpp
@@ -0,0 +1,17 @@
+// Name:
+// Intro to CS 
+// Period 1
+// Jarren Style 
+// 2019- 10-03- Classwork 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "Enter your favorite number";
+	int favoritenumber;
+	std::cin >> favoritenumber;
+	std::cout << "Cool," << favoritenumber << " is a good number but i dead did not ask" << std::endl;
+
+}
